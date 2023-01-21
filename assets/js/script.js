64e7d2213dbd91d9b94fd06a3252138b120711e9
Alex Ainslie <alex.ainslie99@gmail.com>
2023-01-21 17:07:17 +0000

    Created function to create all rows

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,40 @@
 // Dom Elements
 var currentDayEl = $("#currentDay");
+var containerEl = $("#containerEl");
 
 // Moments.js to make Date at top of screen
 var today = moment();
 currentDayEl.text(today.format("dddd, D MMMM YYYY"));
+
+// Making all rows
+for (i = 0; i < 9; i++){
+
+    // Create Row
+    var hourRowEl = $("<div>");
+    hourRowEl.addClass("row time-block");
+    hourRowEl.attr("data-time", [i]);
+    containerEl.append(hourRowEl);
+
+    // Adding hour colum to row
+    var hourColEl = $("<div>");
+    hourColEl.addClass("col hour");
+    hourColEl.text([i]);
+    hourRowEl.append(hourColEl);
+
+    // Adding text colum to row
+    var textColEl = $("<textarea>");
+    textColEl.addClass("col-10 description");
+    hourRowEl.append(textColEl);
+
+    // Adding Submit Button to row
+    var submitColEl = $("<button>");
+    submitColEl.addClass("col saveBtn");
+    submitColEl.text("Submit");
+    hourRowEl.append(submitColEl);
+}
+
+
+
 
 // Getting a time variable
 let timeLive = moment().format("H");
@@ -21,7 +52,3 @@ function timeColor(){
         $("#9amText").addClass("future");
     }
 }
-
-timeColor();
-console.log(timeLive);
-console.log($("#9am").text());
