16eb42b83af2788bf4e14ed8043df4316d8b508c
Alex Ainslie <alex.ainslie99@gmail.com>
2023-01-21 16:45:44 +0000

    added basic html of row and function to tell current time

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,24 @@ var currentDayEl = $("#currentDay");
 // Moments.js to make Date at top of screen
 var today = moment();
 currentDayEl.text(today.format("dddd, D MMMM YYYY"));
+
+// Getting a time variable
+let timeLive = moment().format("H");
+
+
+// Setting attributes dependant on time
+function timeColor(){
+    if(timeLive > $("#9am").text()){
+        $("#9amText").addClass("past");
+    }
+    else if(timeLive == $("#9am").text()){
+        $("#9amText").addClass("present");
+    }
+    else if(timeLive < $("#9am").text()){
+        $("#9amText").addClass("future");
+    }
+}
+
+timeColor();
+console.log(timeLive);
+console.log($("#9am").text());
